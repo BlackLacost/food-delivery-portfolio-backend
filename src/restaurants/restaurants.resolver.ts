ce783b185b30da79ae3f391ad0ae62eaa1a8f8f8
Ilya Lisin <blacklacost@gmail.com>
2022-09-26 14:53:39 +0300

    ArgumentTypes

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,4 +1,5 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateRestaurantDto } from 'src/restaurants/dtos/create-restaurant.dto';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 
 @Resolver(() => Restaurant)
@@ -7,5 +8,13 @@ export class RestaurantResolver {
   restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
     console.log(veganOnly);
     return [];
+  }
+
+  @Mutation(() => Boolean)
+  createRestaurant(
+    @Args() createRestaurantInput: CreateRestaurantDto,
+  ): boolean {
+    console.log(createRestaurantInput);
+    return true;
   }
 }
