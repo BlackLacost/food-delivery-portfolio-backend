9f9e19f29e1b3345066c7dcd95805be6f4f80b15
Ilya Lisin <blacklacost@gmail.com>
2022-09-26 13:57:51 +0300

    Arguments

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,13 +1,11 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 
-@Resolver()
+@Resolver(() => Restaurant)
 export class RestaurantResolver {
-  @Query(() => Restaurant)
-  myRestaurant(): Restaurant {
-    return {
-      name: 'Ilya',
-      isGood: true,
-    };
+  @Query(() => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    console.log(veganOnly);
+    return [];
   }
 }
