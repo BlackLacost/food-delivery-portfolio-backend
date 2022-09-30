41902e56e5020a369b5a26d43d83924245e943f5
Ilya Lisin <blacklacost@gmail.com>
2022-09-30 18:35:33 +0300

    userProfile & me Test

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,7 +1,9 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
 import * as request from 'supertest';
-import { Connection } from 'typeorm';
+import { Connection, Repository } from 'typeorm';
 import { AppModule } from '../src/app.module';
 
 jest.mock('nodemailer', () => ({
@@ -16,13 +18,16 @@ const testUser = {
   email: 'blacklacost@gmail.com',
   password: 'qwe',
 };
-type User = typeof testUser;
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
+  let usersRepository: Repository<User>;
+  let jwtToken: string;
 
   const baseTest = () => request(app.getHttpServer()).post(GRAPHQL_ENDPOINT);
   const publicTest = (query: string) => baseTest().send({ query });
+  const privateTest = (query: string, token: string) =>
+    baseTest().set('X-JWT', token).send({ query });
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -30,6 +35,7 @@ describe('UserModule (e2e)', () => {
     }).compile();
 
     app = module.createNestApplication();
+    usersRepository = module.get(getRepositoryToken(User));
     await app.init();
   });
 
@@ -39,7 +45,7 @@ describe('UserModule (e2e)', () => {
   });
 
   describe('createAccount', () => {
-    const mutation = ({ email, password }: User) => `
+    const mutation = ({ email, password }) => `
           mutation {
             createAccount(input: {
               email: "${email}",
@@ -79,7 +85,7 @@ describe('UserModule (e2e)', () => {
   });
 
   describe('login', () => {
-    const mutation = ({ email, password }: User) => `
+    const mutation = ({ email, password }) => `
           mutation {
             login(input: {
               email: "${email}",
@@ -103,6 +109,7 @@ describe('UserModule (e2e)', () => {
       expect(login.ok).toEqual(true);
       expect(login.error).toEqual(null);
       expect(login.token).toEqual(expect.any(String));
+      jwtToken = login.token;
     });
 
     it('should not be able to login with wrong password', async () => {
@@ -120,8 +127,89 @@ describe('UserModule (e2e)', () => {
     });
   });
 
-  it.todo('userProfile');
-  it.todo('me');
+  describe('userProfile', () => {
+    let userId: number;
+    const query = (userId: number) => `
+    {
+      userProfile(userId: ${userId}) {
+        ok,
+        error,
+        user {
+          id,
+        }
+      }
+    }
+    `;
+
+    beforeAll(async () => {
+      const user = await usersRepository.findOneBy({ email: testUser.email });
+      userId = user.id;
+    });
+
+    it("should see a user's profile", async () => {
+      const {
+        status,
+        body: {
+          data: { userProfile },
+        },
+      } = await privateTest(query(userId), jwtToken);
+
+      expect(status).toEqual(200);
+      expect(userProfile.ok).toEqual(true);
+      expect(userProfile.error).toEqual(null);
+      expect(userProfile.user.id).toEqual(userId);
+    });
+
+    it('should not find a profile', async () => {
+      const notExistsUserId = 666;
+
+      const {
+        status,
+        body: {
+          data: { userProfile },
+        },
+      } = await privateTest(query(notExistsUserId), jwtToken);
+      console.log(userProfile);
+
+      expect(status).toEqual(200);
+      expect(userProfile.ok).toEqual(false);
+      expect(userProfile.error).toEqual(expect.any(String));
+      expect(userProfile.user).toEqual(null);
+    });
+  });
+
+  describe('me', () => {
+    const query = () => `
+    {
+      me {
+        email
+      }
+    }
+    `;
+
+    it('should find my profile', async () => {
+      const {
+        status,
+        body: {
+          data: { me },
+        },
+      } = await privateTest(query(), jwtToken);
+
+      expect(status).toEqual(200);
+      expect(me.email).toEqual(testUser.email);
+    });
+
+    it('should not allow logged out user', async () => {
+      const {
+        status,
+        body: { errors },
+      } = await publicTest(query());
+
+      expect(status).toEqual(200);
+      expect(errors[0].message).toEqual('Forbidden resource');
+    });
+  });
+
   it.todo('verifyEmail');
   it.todo('editProfile');
 });
